4fc58e53ae4757959ac142fbef0dcda5e2351376
Marcin Bigos <marcin.bigos99@gmail.com>
2023-02-09 22:23:24 +0100

    propose survey

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use dotenv::dotenv;
 use rust_i18n::t;
 use serenity::async_trait;
+use serenity::builder::CreateEmbedAuthor;
 use serenity::model::gateway::Ready;
-use serenity::model::prelude::Message;
+use serenity::model::prelude::{Message, ReactionType};
+use serenity::model::user::User;
 use serenity::prelude::*;
 use std::env;
 
@@ -20,17 +22,42 @@ impl EventHandler for Handler {
         if new_message.author.bot {
             return;
         }
-        new_message
+        let create_vote_channel_id: u64 = 636638300731604996;
+        if new_message.channel_id.as_u64() != &create_vote_channel_id {
+            return;
+        }
+        let sent_message_id = new_message
             .channel_id
             .send_message(&ctx.http, |m| {
                 m.embed(|e| {
-                    e.title(t!("hello", locale = "pl"))
-                        .description("description")
+                    e.author(|a| map_author_to_embed(a, new_message.author))
+                        .title(t!("vote_asked.title", locale = "pl", approve_emoji = "✅"))
+                        .description(&new_message.content)
                 })
             })
             .await
-            .expect("Error sending message");
+            .expect("Error sending message")
+            .id;
+
+        new_message
+            .channel_id
+            .create_reaction(
+                &ctx.http,
+                sent_message_id,
+                ReactionType::Unicode("✅".to_string()),
+            )
+            .await
+            .expect("Error adding reaction to message");
     }
+}
+
+fn map_author_to_embed(builder: &mut CreateEmbedAuthor, author: User) -> &mut CreateEmbedAuthor {
+    builder
+        .name(format!("{}#{}", &author.name, &author.discriminator))
+        .icon_url(match &author.avatar_url() {
+            Some(x) => x.to_owned(),
+            None => author.default_avatar_url(),
+        })
 }
 
 #[tokio::main]

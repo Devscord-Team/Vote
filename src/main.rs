755c5f071fa30fd20b4eaccc18b39ac43c256b80
Marcin Bigos <marcin.bigos99@gmail.com>
2023-02-09 20:43:18 +0100

    react to messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::model::prelude::Message;
+use serenity::prelude::*;
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+
+    async fn message(&self, ctx: Context, new_message: Message) {
+        if new_message.author.bot {
+            return;
+        }
+        new_message
+            .channel_id
+            .say(
+                &ctx.http,
+                format!(
+                    "Message: {}, Author: {}",
+                    new_message.content, new_message.author.name
+                ),
+            )
+            .await
+            .expect("Error sending message");
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    /* BEFORE START create .env file in root path with content
+    DISCORDTOKEN=your_bot_token
+    */
+    dotenv().ok();
+
+    let token = env::var("DISCORDTOKEN").unwrap();
+    let mut client = Client::builder(
+        token,
+        GatewayIntents::MESSAGE_CONTENT
+            | GatewayIntents::GUILD_PRESENCES
+            | GatewayIntents::GUILD_MESSAGES
+            | GatewayIntents::GUILD_MESSAGE_REACTIONS,
+    )
+    .event_handler(Handler)
+    .await
+    .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
